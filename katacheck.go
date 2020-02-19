711c8a314c2b787bcac5345049fa922638022ad2
Reynaldo Fang <reynaldfang@hotmail.com>
2020-02-19 19:00:26 +0700

    test2

--- katacheck.go
+++ katacheck.go
@@ -1,7 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
+ 	"bufio"
+ 	"os"
+ 	"strings"
+ 	"fmt"
 )
+
+func main() {
+ scanner := bufio.NewScanner(os.Stdin)
+ for scanner.Scan() {
+  words := strings.Split(scanner.Text(), " ")
+  m := make(map[string]int) 
+     for _, word := range words { 
+        m[word] += 1 
+    } 
+    fmt.Println(m)
+ }
+}
